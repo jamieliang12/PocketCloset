e337e2a7a422ad70423ac5dd0436eaed26b5b737
jamieliang12 <jamieltx@gmail.com>
2025-12-01 16:01:21 -0500

    Initial Commit

---
+++ PocketCloset/PocketClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketClosetApp.swift
+//  PocketCloset
+//
+//  Created by jamie liang on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PocketClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
